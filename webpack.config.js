47242c381bc5ff11c65518b9177d7e9233a20a65
Noah Cowan <nsncowan@gmail.com>
2023-01-12 11:03:10 -0800

    update template with placeholder project names.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: '[HTML-title-here]',
       template: './src/index.html',
       inject: 'body'
     })
